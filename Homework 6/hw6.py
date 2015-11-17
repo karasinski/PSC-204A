30ee1d5d39f7c69e484fca5dc1acff36dd917602
John Karasinski <karasinski@gmail.com>
2015-11-16 20:50:40 -0800

    finish hw6 problem 6

--- Homework 6/hw6.py
+++ Homework 6/hw6.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
-
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('hw06data.csv')
 
@@ -18,25 +18,25 @@ print(df.groupby(('biosex', 'workout')).apply(len))
 
 #What are the mean and standard deviation of percent gain overall,
 #for each condition, and for all condition interactions?
-print(pd.DataFrame({'mean': df.mean(),
-                    'std': df.std()}))
-print(df.groupby('biosex').agg(['mean', 'std']))
-print(df.groupby('workout').agg(['mean', 'std']))
+overall = (pd.DataFrame({'mean': df.mean(), 'std': df.std()}))
+overall.index = ['overall']
+biosex = (df.groupby('biosex').pctgain.agg(['mean', 'std']))
+workout = (df.groupby('workout').pctgain.agg(['mean', 'std']))
+single_factors = pd.concat((overall, biosex, workout))
+print(single_factors)
 print(df.groupby(('biosex', 'workout')).agg(['mean', 'std']))
 
 #3.
 #a. Conduct a factorial ANOVA testing the main effect of biological sex, lifting condition, and their interaction.
-lm = ols("pctgain ~ C(biosex) + C(workout) + C(biosex)*C(workout)", data=df).fit()
-print(lm.summary())
-print(anova_lm(lm))
+print(anova_lm(ols("pctgain ~ C(biosex)*C(workout)", df).fit(), typ=2))
 
 #b. What conclusions do you reach? Explain these in terms of the study. Make sure you incorporate reporting of your statistical analyses into your conclusion.
 
 
 
-plt.figure(figsize=(8,6))
-fig = interaction_plot(df['workout'], df['biosex'], df['pctgain'],
-                       colors=['red', 'blue'], markers=['D','^'],
-                       ms=10, ax=plt.gca())
-plt.show()
+#plt.figure(figsize=(8,6))
+#fig = interaction_plot(df['workout'], df['biosex'], df['pctgain'],
+#                       colors=['red', 'blue'], markers=['D','^'],
+#                       ms=10, ax=plt.gca())
+#plt.show()
 
