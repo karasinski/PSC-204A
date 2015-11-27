acb287341f86dd8008b85075e3c7a347dfa72006
John Karasinski <karasinski@gmail.com>
2015-11-26 23:40:39 -0800

    more extra credit work

--- ExtraCredit/Option 1 Computation/dice.py
+++ ExtraCredit/Option 1 Computation/dice.py
@@ -19,16 +19,23 @@ def throw(AdX, n=1, success_min=None, botching=False):
     # Pull the values for your roll.
     A, X = np.array(AdX.split('d'), dtype=int)
 
-    if n == 1:
+    if n == 1 and success_min is None:
         # Just the results of the dice roll.
-        return dice(A, X)
+        if A == 1:
+            return dice(A, X)[0][0]
+        else:
+            return dice(A, X).sum()
     elif success_min is None:
         # Statistics on the result of many dice rolls.
         scores = dice(A, X, n).sum(axis=1)
-    elif success_min is not None and not botching:
+    elif success_min and not botching:
         # Statistics of the results of many dice rolls with a minimum value for
         # success.
         scores = (dice(A, X, n) >= success_min).sum(axis=1)
+
+        # Just the number of successes for this roll.
+        if n == 1:
+            return scores[0]
     elif botching:
         # Statistics of the results of many dice rolls with a minimum value for
         # success and the possibility of botching.
@@ -38,11 +45,48 @@ def throw(AdX, n=1, success_min=None, botching=False):
         d[d > 0] = 1
         scores = d.sum(axis=1)
         scores[scores < 0] = -1
+        if n == 1:
+            return scores[0]
     else:
         raise ValueError('Sorry, these values are not supported.')
 
     # Generate the statistics.
     s = pd.Series(scores).value_counts().reset_index()
     s.columns = ['Value', 'Count']
+    s['Frequency'] = s['Count']/s['Count'].sum() * 100
     s = s.sort('Value')
     return s
+
+
+def main():
+    # %timeit throw('1d20')
+    # 100000 loops, best of 3: 10.4 µs per loop
+
+    # %timeit throw('2d6')
+    # 100000 loops, best of 3: 15.4 µs per loop
+
+    # %timeit throw('4d10', success_min=5)
+    # 10000 loops, best of 3: 20.4 µs per loop
+
+    # %timeit throw('4d10', success_min=5, botching=True)
+    # 10000 loops, best of 3: 32.3 µs per loop
+
+    a = throw('1d6', n=1000)
+    b = throw('2d6', n=1000)
+    c = throw('6d10', n=1000, success_min=6)
+    d = throw('6d6', n=1000, success_min=6, botching=True)
+
+    print('a. Roll 1d6 1,000 times, frequency of each outcome?\n')
+    print(a)
+
+    print('b. Roll 2d6 1,000 times, frequency of each outcome? Use the summed scores of each die as your outcome.\n')
+    print(b)
+
+    print('c. Roll 6d10 1,000 times, frequency of each outcome? Treat a score of 6 or better as a success and scores less than 6 as nothing, the outcome is total number of successes (include the frequency of no successes).\n')
+    print(c)
+
+    print('d. Same as (c) above except now scores of 1 count as a botch. Each botch is subtracted from each success. If there are more botches than success (i.e., -1 successes) then the overall outcome is a botch. Now depict the frequency of botching, and total number of successes (as before, include the frequency of no successes).\n')
+    print(d)
+
+if __name__ == '__main__':
+    main()
