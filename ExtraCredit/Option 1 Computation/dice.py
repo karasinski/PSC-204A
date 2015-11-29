52c81dc72dc84e99798b18a0a0cabcc0ee5cf56a
John Karasinski <karasinski@gmail.com>
2015-11-28 17:31:01 -0800

    additional ec work

--- ExtraCredit/Option 1 Computation/dice.py
+++ ExtraCredit/Option 1 Computation/dice.py
@@ -12,7 +12,7 @@ def dice(A, X, n=1):
     return np.random.randint(low=1, high=X+1, size=(n, A))
 
 
-def throw(AdX, n=1, success_min=None, botching=False):
+def throw(AdX, n=1, success_min=None, botching=False, stats=False, plot=False):
     '''
     Throw dice with optional repetitions, minimum value for success, and
     botching.
@@ -52,26 +52,34 @@ def throw(AdX, n=1, success_min=None, botching=False):
     else:
         raise ValueError('Sorry, these values are not supported.')
 
+    if plot:
+        filename = AdX
+        if success_min: filename += '_min' + str(success_min)
+        if botching: filename += '_botching'
+        statistics(scores, plot=True, filename=filename)
+    if stats:
+        return statistics(scores)
+
     return scores
 
 
-def statistics(scores, plot=False, save=False):
+def statistics(scores, plot=False, filename=False):
     # Generate the statistics.
     s = pd.Series(scores).value_counts().reset_index()
     s.columns = ['Value', 'Count']
     s['Frequency'] = s['Count']/s['Count'].sum() * 100
     s = s.sort('Value')
 
-    print(s)
     if plot:
         sns.countplot(scores, order=np.unique(scores))
         plt.xlabel('Value')
         plt.ylabel('Count')
         plt.tight_layout()
-        if save:
-            plt.savefig(save + '.pdf')
+        if filename:
+            plt.savefig(filename + '.pdf')
+        else:
+            plt.show()
         plt.close('all')
-        #plt.show()
     return s
 
 
@@ -89,16 +97,16 @@ def main():
     # 10000 loops, best of 3: 32.3 µs per loop
 
     print('a. Roll 1d6 1,000 times, frequency of each outcome?\n')
-    statistics(throw('1d6', n=1000), plot=True, save='1d6')
+    print(throw('1d6', n=1000, plot=True, stats=True))
 
     print('b. Roll 2d6 1,000 times, frequency of each outcome? Use the summed scores of each die as your outcome.\n')
-    statistics(throw('2d6', n=1000), plot=True, save='2d6')
+    print(throw('2d6', n=1000, plot=True, stats=True))
 
     print('c. Roll 6d10 1,000 times, frequency of each outcome? Treat a score of 6 or better as a success and scores less than 6 as nothing, the outcome is total number of successes (include the frequency of no successes).\n')
-    statistics(throw('6d10', n=1000, success_min=6), plot=True, save='6d10>6')
+    print(throw('6d10', n=1000, success_min=6, plot=True, stats=True))
 
     print('d. Same as (c) above except now scores of 1 count as a botch. Each botch is subtracted from each success. If there are more botches than success (i.e., -1 successes) then the overall outcome is a botch. Now depict the frequency of botching, and total number of successes (as before, include the frequency of no successes).\n')
-    statistics(throw('6d6', n=1000, success_min=6, botching=True), plot=True, save='6d10>6-botching')
+    print(throw('6d6', n=1000, success_min=6, botching=True, plot=True, stats=True))
 
 if __name__ == '__main__':
     main()
