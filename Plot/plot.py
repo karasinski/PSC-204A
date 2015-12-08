0dd44a381922bfa847c78374e66b5f4e6104ae1a
John Karasinski <karasinski@gmail.com>
2015-12-07 18:55:05 -0800

    finish

--- Plot/plot.py
+++ Plot/plot.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -45,19 +46,40 @@ def load_data():
 def plot(df2, df3):
     sns.set(style="white", color_codes=True)
     f, ax = sns.plt.subplots()
-    sns.kdeplot(df2.bmi, ax=ax, shade=True)
-    sns.kdeplot(df3.bmi, ax=ax, shade=True)
-    plt.legend(['Wave 2', 'Wave 3'])
-    plt.xlim(10, 60)
-    #plt.axvline(18.5, color='k', linewidth=1)#, ls='dashed')
-    plt.axvline(25, color='k', linewidth=1)#, ls='dashed')
-    plt.axvline(30, color='k', linewidth=1)#, ls='dashed')
-    #plt.axvspan(25, 30, color='red', alpha=0.05)
-    #plt.axvspan(30, 60, color='red', alpha=0.1)
-    plt.ylabel('Frequency')
-    plt.xlabel('BMI (kg/m$^2$)')
+    sns.kdeplot(df2.bmi, ax=ax, shade=True, color='k', gridsize=10000, clip=(15, 45))
+    sns.kdeplot(df3.bmi, ax=ax, shade=True, color='k', ls='dashed', gridsize=10000, clip=(15, 45))
+
+    plt.legend(['Wave 2 (1996; ages 13-20 y)', 'Wave 3 (2001; ages 19-26 y)'], fontsize=15)
+    plt.xlim(15, 45)
+
+    ax.annotate('10.9%', xy=(25, .02), xytext=(30.5, .005), color='k')
+    ax.annotate('22.1%', xy=(25, .04), xytext=(30.5, .02), color='k')
+
+    y1 = ax.lines[0].get_ydata()
+    x1 = ax.lines[0].get_xdata()
+    x_mask1 = np.ma.masked_less_equal(x1, 30).mask
+    y_masked1 = np.ma.masked_array(y1, x_mask1)
+
+    y2 = ax.lines[1].get_ydata()
+    x2 = ax.lines[1].get_xdata()
+    x_mask2 = np.ma.masked_less_equal(x2, 30).mask
+    y_masked2 = np.ma.masked_array(y2, x_mask2)
+
+    ax.fill_between(x2, np.zeros_like(y2), y_masked2, facecolor='red', interpolate=True, alpha=0.5)
+    ax.fill_between(x1, np.zeros_like(y1), y_masked1, facecolor='white', interpolate=True)
+    ax.fill_between(x2, np.zeros_like(y2), y_masked2, facecolor='red', interpolate=True, alpha=0.25)
+    #ax.fill_between(x1, np.zeros_like(y1), y_masked1, facecolor='k', interpolate=True, alpha=0.5)
+
+    plt.vlines(x=30, ymin=0, ymax=0.0398, color='k', linewidth=2, alpha=1)#, ls='dashed')
+
+    plt.xticks(size=15)
+    plt.yticks(size=15)
+
+    plt.ylabel('Frequency', fontsize=15)
+    plt.xlabel('BMI (kg/m$^2$)', fontsize=15)
     plt.tight_layout()
-    plt.show()
+    #plt.show()
+    plt.savefig('plot.pdf')
 
 
 if __name__ == "__main__":
